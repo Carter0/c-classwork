472b4e73c25be305e59045a5df049a95c0cb306d
Carter Weinberg <carterweinberg@login-students.ccs.neu.edu>
2018-09-11 13:25:14 -0400

    adding hw

--- starter/fizzbuzz.cpp
+++ starter/fizzbuzz.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+using namespace std;
 
-int
-main()
+int main()
 {
-  // TODO
+  	int x;
+	cout << "Enter a value for x" << endl;
+	cin >> x;
+	
+
+	int y;
+	cout << "Enter a value for y" << endl; 
+	cin >>  y;
+
+
+
+	for (int i = 1; i <= 20; ++i) {
+			if (x % i == 0  && y % i  == 0) {
+				cout << "FizzBuzz" << endl;
+			}
+			else if(x % i == 0) {
+				cout << "Fizz" << endl;
+			} else if (y % i  == 0) {
+				cout << "Buzz" << endl;
+			} else {
+				cout << i << endl;
+			}
+		}
+	
 
   return 0;
 }
