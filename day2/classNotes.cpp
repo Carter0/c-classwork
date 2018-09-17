ad74d4e1371f3ee7ae626af7e507ea54ca0d520f
Carter Weinberg <carterweinberg@login-students.ccs.neu.edu>
2018-09-17 14:27:16 -0400

    adding more stuff

---
+++ day2/classNotes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int x = 1;
+	cout << x << endl;
+
+	#
+	//make command -> recipies for how to assemble a program
+	//there are make files for the assignment
+	//basically just a text file with the proper stuff in it
+	//will have to google contents of text file later
+	//start looking up stuff with the make commmand.
+	//
+	
+
+	auto y = 1.5;
+	cout << "y is " << y << endl;
+	cout << "x and y are " << x << " and " << y << endl;
+
+	
+}
+
+
---
+++ input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+	cout << "Enter your name pls!" << endl;
+	string name;
+	cin >> name;
+	cout << "Hello " << name << endl;
+}
+
+
