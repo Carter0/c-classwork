5c0a8dfb6bd46c19c4d00daef4c2261ff509dec2
Carter Weinberg <weinbergcarter@gmail.com>
2018-11-08 18:20:29 -0500

    Finished some of the hw

--- UnitTests /IntArray.cxxtest.hpp
+++ UnitTests /IntArray.cxxtest.hpp
@@ -13,10 +13,104 @@ class IntArrayTestSuite : public CxxTest::TestSuite
         void test_REMOVEME() {}
 
         void test_DoubleEquals() {
-            IntArray test1();
-            IntArray test2();
+            IntArray test1;
+            IntArray test2;
             TS_ASSERT(test1 == test2);
         }
+
+        void test_NotEquals() {
+            IntArray test1;
+            IntArray test2(90);
+            TS_ASSERT(test1 != test2);
+        }
+
+        void test_CopyConstructor() {
+            IntArray test1;
+            IntArray test2(test1);
+            TS_ASSERT(test1 == test2);
+        }
+
+
+        void test_ListConstructor() {
+            auto testList = { 1, 2, 3};
+            IntArray test1(testList);
+            TS_ASSERT_EQUALS(3, test1.size());
+        } 
+
+        void test_MoveConstructor() {
+            IntArray test(IntArray(sizeof(3)));
+            TS_ASSERT_EQUALS(test.size(), 4);
+        }
+
+        void test_destructor() {
+            IntArray * test1;
+            test1 = new IntArray(sizeof(3));
+            TS_ASSERT_EQUALS(4, test1->size());
+            test1->~IntArray();
+            TS_ASSERT(test1->size() != 3);
+        }
+
+        void test_ptr() {
+            auto testList = {0, 1, 2, 3};
+            IntArray test1(testList); 
+            const int * temp = test1.ptr();
+            TS_ASSERT_EQUALS(2, temp[2]);
+        }
+
+
+        void test_size() {
+            IntArray test1;
+            TS_ASSERT_EQUALS(test1.size(), 0);
+        }
+
+        void test_Assignment() {
+            IntArray * test1;
+            test1 = new IntArray(3);
+            TS_ASSERT_EQUALS(3, test1->size());
+            test1->~IntArray();
+            TS_ASSERT(test1->size() != 2);
+        }
+
+        void test_Assignment2() {
+            IntArray test1(3);
+            IntArray test2 = std::move(test1);
+            TS_ASSERT_EQUALS(test2.size(), 3);
+        }
+
+        void test_IndexGetter() {
+            IntArray test1(2);
+            test1[1] = 5;
+            TS_ASSERT_EQUALS(test1[1], 5);
+        }
+
+        void test_ConstIndexGetter() {
+            auto testList = {0, 1, 2, 3};
+            IntArray test1(testList);
+            const size_t index = 3;
+            TS_ASSERT_EQUALS(test1[index], 3);  
+
+        }
+
+        void test_IndexOutOfBounds() {
+            IntArray test1;
+            TS_ASSERT_THROWS_ANYTHING(test1[5]);
+        }
+
+        void test_IndexOut2() {
+            IntArray test1;
+            const size_t index = 5;
+            TS_ASSERT_THROWS_ANYTHING(test1[index]);
+
+        } 
+
+        void test_MoveDeletionStuff() {
+            IntArray test1(5);
+            test1[3] = 9;
+            IntArray test2(std::move(test1));
+            TS_ASSERT_EQUALS(test1.size(), 0);
+        }
+
+
 };
 
 #endif
--- UnitTests /IntArrayAlgorithms.cpp
+++ UnitTests /IntArrayAlgorithms.cpp
@@ -2,24 +2,42 @@
 #include "IntArray.hpp"
 
 void IntArrayAlgorithms::swap(IntArray & lhs, IntArray & rhs) {
-  // TODO
+    int* firstArray = lhs.ptr();
+    lhs.ptr() = rhs.ptr();
+    rhs.ptr() = firstArray;
 }
 
 bool IntArrayAlgorithms::contains(const IntArray & arr, int val) {
-  // TODO
-  return false;
+    for(int i = 0; i < arr.size(); i++) {
+        if(arr[i] == val) {
+            return true;
+        }
+    }
+    return false;
 }
 
 void IntArrayAlgorithms::sort(IntArray & arr) {
-  // TODO
+    // TODO
 }
 
 int IntArrayAlgorithms::sum(const IntArray & arr) {
-  // TODO
-  return 0;
+    if (arr.size() == 0) {
+        return 0;
+    } else {
+        int sum = 0;
+        for(int i = 0; i < arr.size(); i++) {
+            sum+= arr[i];
+        }
+        return sum;
+    }
 }
 
 int IntArrayAlgorithms::product(const IntArray & arr) {
-  // TODO
-  return 1;
+
+    int product = 1;
+    for(int i = 0; i < arr.size(); i++) {
+        product *= arr[i];
+    }
+    return product;
+
 }
