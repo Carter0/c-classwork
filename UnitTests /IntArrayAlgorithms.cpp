8ef551c43492f5518a73295d384779c321ce05d0
Carter Weinberg <weinbergcarter@gmail.com>
2018-11-09 17:55:27 -0500

    Finishing hw

--- UnitTests /IntArrayAlgorithms.cpp
+++ UnitTests /IntArrayAlgorithms.cpp
@@ -1,11 +1,31 @@
 #include "IntArrayAlgorithms.hpp"
 #include "IntArray.hpp"
+#include <iostream>
 
 void IntArrayAlgorithms::swap(IntArray & lhs, IntArray & rhs) {
-    int* firstArray = lhs.ptr();
-    lhs.ptr() = rhs.ptr();
-    rhs.ptr() = firstArray;
+    if(lhs!=rhs) {
+        //allocate a temp array on the heap
+        int* temp = new int[lhs.size()];
+        for(int i = 0; i < lhs.size(); i++) {
+            temp[i] = lhs[i];
+        } 
+
+        for(int i = 0; i < lhs.size(); i++){
+            if(i < rhs.size()) {
+                lhs[i] = rhs[i];
+            }
+        }
+
+        for(int i = 0; i < rhs.size(); i++){
+            if(i < lhs.size()) {
+                rhs[i] = temp[i];
+            }
+        }
+
+        delete[] temp;
+    }
 }
+
 
 bool IntArrayAlgorithms::contains(const IntArray & arr, int val) {
     for(int i = 0; i < arr.size(); i++) {
@@ -16,8 +36,24 @@ bool IntArrayAlgorithms::contains(const IntArray & arr, int val) {
     return false;
 }
 
+//Note, heavily influenced from two insertion sort articles online. 
+//I googled a lot of things in order to see how to implement this
 void IntArrayAlgorithms::sort(IntArray & arr) {
-    // TODO
+    int toSwap; 
+    int previous;
+
+    //find the current integer to swap back
+    for (int i = 1; i < arr.size(); i++) { 
+        toSwap = arr[i]; 
+        previous = i-1; 
+        
+        //compare the integer to swap back and swap if needed
+        while (previous >= 0 && arr[previous] > toSwap) { 
+            arr[previous+1] = arr[previous]; 
+            previous = previous-1; 
+        } 
+        arr[previous+1] = toSwap; 
+    }
 }
 
 int IntArrayAlgorithms::sum(const IntArray & arr) {
@@ -33,6 +69,9 @@ int IntArrayAlgorithms::sum(const IntArray & arr) {
 }
 
 int IntArrayAlgorithms::product(const IntArray & arr) {
+    if(arr.size() == 0) {
+        return 0;
+    }
 
     int product = 1;
     for(int i = 0; i < arr.size(); i++) {
--- UnitTests /IntArrayAlgorithms.cxxtest.hpp
+++ UnitTests /IntArrayAlgorithms.cxxtest.hpp
@@ -7,10 +7,62 @@
 
 class IntArrayAlgorithmsTestSuite : public CxxTest::TestSuite
 {
-public:
+    public:
 
-  // TODO
-  void test_REMOVEME() {}
+        void test_REMOVEME() {}
+
+
+        void test_swap() {
+            IntArray temp1 = {2, 2, 2, 2};
+            IntArray temp2 = {3, 3, 3, 3};
+            IntArrayAlgorithms test;
+            test.swap(temp1, temp2);
+            for(int i = 0; i < temp1.size(); i++) {
+                TS_ASSERT_EQUALS(temp1[i], 3);
+                TS_ASSERT_EQUALS(temp2[i], 2);
+            }
+        }
+
+        void test_sum() {
+            IntArrayAlgorithms test;
+            IntArray temp1 = {1, 2, 3};
+            TS_ASSERT_EQUALS(test.sum(temp1), 6);
+        }
+
+        void test_sum2() {
+            IntArrayAlgorithms test;
+            IntArray temp1;
+            TS_ASSERT_EQUALS(test.sum(temp1), 0);
+        }
+
+        void test_product() {
+            IntArrayAlgorithms test;
+            IntArray temp1;
+            TS_ASSERT_EQUALS(test.product(temp1), 0);
+        }
+
+        void test_product2() {
+            IntArrayAlgorithms test;
+            IntArray temp1 = {3, 1, 2 ,3};
+            TS_ASSERT_EQUALS(test.product(temp1), 18);
+        }
+
+
+        void test_sort() {
+            IntArrayAlgorithms test;
+            IntArray temp1 = {2, 3, 0, 1};
+            test.sort(temp1);
+            for(int i = 0; i < temp1.size(); i++) {
+                TS_ASSERT_EQUALS(temp1[i], i);
+            }
+        }
+        void test_contains() {
+            IntArrayAlgorithms test;
+            IntArray temp1 = {1, 2, 3};
+            TS_ASSERT_EQUALS(test.contains(temp1, 1), 1);
+            TS_ASSERT_EQUALS(test.contains(temp1, 9), 0);
+            
+        }
 
 };
 
