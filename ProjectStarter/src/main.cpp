7119c56e573f4562233ea24361d7e7bb0711db39
Carter.Weinberg <weinbergcarter@gmail.com>
2018-11-13 14:33:15 -0500

    putting projects together

--- ProjectStarter/src/main.cpp
+++ ProjectStarter/src/main.cpp
@@ -1,9 +1,27 @@
 #include <SDL2/SDL.h>
- 
-int main(int argc, char ** argv)
-{
-  SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Quit();
- 
-  return 0;
+#include <iostream>
+#include <stdio.h>
+#include "Game.h"
+using namespace std;
+
+//create game obj.
+Game *game = nullptr;
+int main(int argc, char *argv[]) {
+
+	//initialize it
+	game = new Game();
+	game->init("First", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 500, false);
+
+	//keep it going until we quit
+	while (game->running()) {
+		game->update();
+		game->render();
+
+	}
+
+	//clean up after
+	game->clean();
+	return 0;
+
 }
+
